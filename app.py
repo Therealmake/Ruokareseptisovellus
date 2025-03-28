cae42d636695b665de9e6679bfa5c425b0e48448
Therealmake <hemmimarkus1@gmail.com>
2025-03-28 16:40:12 +0200

    Add user page

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import sqlite3
 import db
 import config
 import recipes
+import users
 
 from flask import Flask
 from flask import abort, flash, redirect, render_template, request, session
@@ -17,6 +18,14 @@ def require_login():
 def index():
     all_recipes = recipes.get_recipes()
     return render_template("index.html", recipes=all_recipes)
+
+@app.route("/user/<int:user_id>")
+def show_user(user_id):
+    user = users.get_user(user_id)
+    if not user:
+        abort(404)
+    recipes = users.get_recipes(user_id)
+    return render_template("show_user.html", user=user, recipes=recipes)
 
 @app.route("/search_recipe")
 def search_recipe():
